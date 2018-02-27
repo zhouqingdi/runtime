da03edc2ae6baeb08e2761b74aa03fc7f0040207
lifei <lifei@dev.4dq.com>
2018-02-27 14:04:31 +0800

    Initial Commit

---
+++ Runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime
+//
+//  Created by bear on 2018/2/27.
+//  Copyright © 2018年 bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Runtime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Runtime
+//
+//  Created by bear on 2018/2/27.
+//  Copyright © 2018年 bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
